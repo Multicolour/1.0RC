2bfed8ff262b25a8446cf3aa24be1b5e418ce6d6
davemackintosh <me@davemackintosh.co.uk>
2019-04-01 16:41:36 +0100

    Don't need these anymore

--- types/multicolour/content-negotiation.ts
+++
@@ -1,6 +0,0 @@
-import { Multicolour$RequestParserArgs } from "@mc-types/multicolour/route"
-
-export class Multicolour$ContentNegotiator {
-  public parseBody: (body: Multicolour$RequestParserArgs) => Promise<any>
-}
-
