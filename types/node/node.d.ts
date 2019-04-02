a58adcee4456382037173781783723fc40565358
davemackintosh <me@davemackintosh.co.uk>
2019-04-02 21:27:25 +0100

    required because ServerOptions doesnt change the type of createServer.requestListener

--- types/node/node.d.ts
+++ types/node/node.d.ts
@@ -1,3 +1,7 @@
+import { Multicolour$IncomingMessage } from "@lib/server/incoming-message"
+import { EventEmitter } from "events"
+import { ServerResponse, Server } from "http"
+
 // This exists to fix the uncaughtException event not
 // being part of the @types/node thing.
 // @TODO: PR to add that event name.
@@ -5,4 +9,12 @@ declare namespace NodeJS {
   interface Process extends EventEmitter {
     on(event: string, callback: (...args: any[]) => void): this;
   }
+}
+
+declare module "http" {
+  function createServer(options: ServerOptions, requestListener?: (request: Multicolour$IncomingMessage, response: ServerResponse) => void): Server
+}
+
+declare module "https" {
+  function createServer(options: ServerOptions, requestListener?: (request: Multicolour$IncomingMessage, response: ServerResponse) => void): Server
 }
