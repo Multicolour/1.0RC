c9cb31ab925ff5f38ec09f03bdbd3fec2d40c871
davemackintosh <me@davemackintosh.co.uk>
2019-04-12 17:12:05 +0100

    Closer

--- test.ts
+++ test.ts
@@ -1,36 +1,40 @@
 import http from "http"
 import { boyerMooreSearch } from "./lib/content-negotiators/multipart/boyer-moore"
 
+const boundary = "--X-INSOMNIA-BOUNDARY"
+
+function parseBufferData(data: Buffer): number[] {
+  let size = data.length
+  let buffer = data
+  const indexes: number[] = []
+
+  while (size > 0) {
+    const result = boyerMooreSearch(data, boundary)
+
+    if (result !== -1) {
+      const tempBuffer = Buffer.allocUnsafe(buffer.length - (result + boundary.length))
+      indexes.push(result)
+      buffer.copy(
+        tempBuffer,
+        0,
+        result,
+        buffer.length,
+      )
+
+      size = tempBuffer.length
+      buffer = tempBuffer
+    }
+  }
+
+  return indexes
+}
+
 http
   .createServer((request, response) => {
-    const boundary = "--X-INSOMNIA-BOUNDARY"
-    let indexes: number[] = []
 
-    request.on("data", (data: Buffer) => {
-      let size = data.length
-      let buffer = data
-      
-      while (size > 0) {
-        const result = boyerMooreSearch(data, boundary)
-
-        if (result !== -1) {
-          indexes.push(result)
-          buffer = Buffer.copy(Buffer.allocUnsafe(buffer.length - (result + boundary.length)), 0, result, buffer.length))
-          
-        }
-
-        
-      }
-      
-      if (results.size) {
-        indexes = [
-          ...indexes,
-          ...results,
-        ]
-      }
-    })
+    request.on("data", parseBufferData)
     request.on("end", () =>
-      response.end(JSON.stringify(indexes, null, 2)),
+      response.end(),
     )
   })
   .listen(5000)
