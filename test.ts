491933ef35a64509fa9485d7bc1615b132e08719
davemackintosh <me@davemackintosh.co.uk>
2019-04-15 09:32:23 +0100

    Still working on looping the Boyer-Moore algo.
    Seems to be working better now, need to work out why there's junk at the
    beginning of each new buffer instead of it being a specific size with
    only a part of the original buffer in it. Swings and round-abouts.

--- test.ts
+++ test.ts
@@ -10,12 +10,25 @@ function parseBufferData(data: Buffer): number[] {
 
   // As long as there's matches, keep searching.
   while (lastResult !== -1) {
+    // Do the search.
     const result = boyerMooreSearch(buffer, boundary)
-    const newBufferSize = buffer.length - (result + boundary.length)
-    console.log(buffer.length, result)
-    console.log("\n".repeat(3))
-    // console.log("nb:", result, buffer.toString(), newBufferSize, "\n".repeat(2))
 
+    if (result === -1) {
+      break
+    }
+
+    // Get the next offset.
+    const resultOffsetNum = result + boundary.length + 2
+
+    // Calculate how big the next buffer needs to be
+    const newBufferSize = buffer.length - resultOffsetNum
+
+    console.log("r", result,  "bs", buffer.length, "nbs", newBufferSize, "ro", resultOffsetNum)
+    console.log("nb:\n")
+    console.log(JSON.stringify(buffer.toString()), "\n".repeat(2))
+
+    // Check the new buffer will have a positive size.
+    // Exit if it doesn't, there's nowt left to search.
     if (newBufferSize < 0) {
       break
     }
@@ -23,12 +36,18 @@ function parseBufferData(data: Buffer): number[] {
     // Create a new buffer without this match for the next loop.
     const tempBuffer = Buffer.allocUnsafe(newBufferSize)
 
+    // Copy the contents of the current buffer into a new one
+    // using the calculated result offset to shrink it.
     buffer.copy(
       tempBuffer,
-      0,
+      resultOffsetNum,
       0,
       newBufferSize,
     )
+
+    console.log("NEW BUFFER", tempBuffer.length)
+    console.log(tempBuffer.toString())
+    console.log("\n".repeat(3))
 
     // Add this result to our collector.
     indexes.push(result)
@@ -45,8 +64,8 @@ http
   .createServer((request, response) => {
     const bodyParts: number[][] = []
     const ct = request.headers["content-type"] || ""
-    boundary = ct.split(";")[1].split("=")[1]
-    console.log("boundary is:\n%s\n", boundary)
+    boundary = "--" + ct.split(";")[1].split("=")[1]
+    console.log("boundary is:\n%s\n", JSON.stringify(boundary))
     request.on("data", (chunk: Buffer) => bodyParts.push(parseBufferData(chunk)))
     request.on("end", () => {
 
