38e6b22fb82c5931e7bfd789772772056e3fc724
davemackintosh <me@davemackintosh.co.uk>
2019-04-19 20:44:16 +0100

    make sure to test random payloads. failing.

--- tests/boyer-moore-hoorspool.ts
+++ tests/boyer-moore-hoorspool.ts
@@ -13,6 +13,11 @@ import BoyerMooreHorspool from "@lib/content-negotiators/multipart/boyer-moore"
     expected: [0],
   },
   {
+    text: "aab".repeat(3),
+    pattern: "aab",
+    expected: [0, 3, 6],
+  },
+  {
     text: "trusthardtoothbrushes",
     pattern: "tooth",
     expected: [10],
@@ -30,3 +35,23 @@ test("ensuring Boyer Moore Hoorspool algorithm works with static payload: " + pa
 
   })
 })
+
+function getRandomString(length = Math.floor(1000 * Math.random())): string {
+  let out = ""
+  while (length--) {
+    out += String.fromCharCode( 48 + ~~(Math.random() * 42))
+  }
+  return out
+}
+
+for (let testIndex = 0, maxTests = 25; testIndex <= maxTests; testIndex++) {
+  console.log(testIndex)
+  const testText = getRandomString()
+  const expected = Math.floor(Math.random() * testText.length)
+  const pattern = testText.substr(expected, Math.floor(Math.random() * testText.length))
+
+  test("ensuring Boyer Moore Hoorspool algorithm works with random payload" + pattern, () => {
+    const instance = new BoyerMooreHorspool(pattern)
+    expect(instance.search(Buffer.from(testText))).toEqual([expected])
+  })
+}
