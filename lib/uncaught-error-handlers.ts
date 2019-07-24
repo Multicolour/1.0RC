3a8b361e750d4b4504fa358116c4eefdd5dd1518
davemackintosh <me@davemackintosh.co.uk>
2019-07-24 08:59:34 +0100

    Always return promises

--- lib/uncaught-error-handlers.ts
+++ lib/uncaught-error-handlers.ts
@@ -16,7 +16,7 @@ process
     // tslint:disable-next-line:max-line-length
     console.error(new UncaughtError(logID + ": An promise rejection occured, please add a .catch((error: Error) => handler) to your code where the stack below instructs.", reason).prettify())
 
-    uncaughtPromise.then(() => ({
+    return uncaughtPromise.then(() => ({
       statusCode: 500,
       // tslint:disable-next-line:max-line-length
       errors: "An unexpected error has happened that this service tried to recover from. This is a developer problem, please contact the owner of this service and quote this reference so they can find the cause in their server logs. " + logID,
